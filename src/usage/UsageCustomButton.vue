2847759014a4a579af519ee8b78d0f306cba717a
b-s-x <b.s.x.mars@gmail.com>
2021-12-19 17:56:39 +0300

    @/usage/UsageCustomButton: add usage button

---
+++ src/usage/UsageCustomButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import CustomButton from '../components/CustomButton.vue';
+const handlerClick = (event: Event) => console.log('fatality', event);
+</script>
+
+<template>
+  <div class="title">
+    <custom-button
+      key="1"
+      value="111"
+      class="super"
+      type="number"
+      placeholder="Enter your username"
+      @click="handlerClick"
+    >
+      111111
+    </custom-button>
+  </div>
+</template>
+
+<style>
+</style>
