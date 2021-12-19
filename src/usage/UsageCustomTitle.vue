cf5663b347263cb1359fef7dc2d18aa6456ec99e
b-s-x <b.s.x.mars@gmail.com>
2021-12-19 14:49:14 +0300

    add folder usage

---
+++ src/usage/UsageCustomTitle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CustomTitle from '../components/CustomTitle.vue';
+</script>
+
+<template>
+  <custom-title
+    key="1"
+    level="7"
+    required
+  >
+    <span>bsx</span>
+    <template #sub>
+      mars
+    </template>
+  </custom-title>
+</template>
+
+<style>
+</style>
