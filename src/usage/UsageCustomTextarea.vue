b55a0bcbb980f4b7f3c5d5e492e1cc418c2e6af4
b-s-x <b.s.x.mars@gmail.com>
2021-12-19 16:38:11 +0300

    @/usage/UsageCustomTextarea: add usage for custom textarea

---
+++ src/usage/UsageCustomTextarea.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import CustomTextarea from '../components/CustomTextarea.vue';
+const handleInput = value => console.log('fatality', value);
+</script>
+
+<template>
+  <div class="box">
+    <custom-textarea
+      key="1"
+      value=""
+      class="super"
+      type="number"
+      placeholder="Enter your username"
+      @input="handleInput"
+    />
+  </div>
+</template>
+
+<style>
+.box {
+  width: 300px;
+}
+</style>
