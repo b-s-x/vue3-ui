2b6beed54302ebf92fdb079c296f3b0d96520468
b-s-x <b.s.x.mars@gmail.com>
2021-12-19 17:25:06 +0300

    @/usage/UsageCustomSpinner: add usage

---
+++ src/usage/UsageCustomSpinner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import CustomSpinner from '../components/CustomSpinner.vue';
+</script>
+
+<template>
+  <div class="title">
+    <custom-spinner
+      key="1"
+      value="111"
+      class="super"
+    />
+  </div>
+</template>
+
+<style>
+</style>
