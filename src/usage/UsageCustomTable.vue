ec7e31f5dcbf6b3e44ab96d4aa1ceecf2135cad1
b-s-x <b.s.x.mars@gmail.com>
2021-12-24 15:35:46 +0300

    @/usage/UsageCustomTable: change usage

--- src/usage/UsageCustomTable.vue
+++ src/usage/UsageCustomTable.vue
@@ -1,22 +1,39 @@
 <script setup lang="ts">
 import CustomTable from '../components/CustomTable.vue';
+import CustomTd from '../components/CustomTd.vue';
+import CustomTr from '../components/CustomTr.vue';
 </script>
 
 <template>
-  <CustomTable
-    key="1"
-    level="7"
-    required
-  >
-    <tr>
-      11111
-    </tr>
-    <div>
-      22222
-    </div>
-    <tr>
-      3333
-    </tr>
+  <CustomTable>
+    <custom-tr>
+      <custom-td>
+        5555555
+      </custom-td>
+      <custom-td>
+        5555555
+      </custom-td>
+      <custom-td>
+        5555555
+      </custom-td>
+      <custom-td>
+        5555555
+      </custom-td>
+    </custom-tr>
+    <custom-tr>
+      <custom-td>
+        1111111
+      </custom-td>
+      <custom-td>
+        1111111
+      </custom-td>
+      <custom-td>
+        1111111
+      </custom-td>
+      <custom-td>
+        1111111
+      </custom-td>
+    </custom-tr>
   </CustomTable>
 </template>
 
