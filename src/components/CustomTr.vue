846e437bf062bac878f5c9f1bdb4294442b3dd16
b-s-x <b.s.x.mars@gmail.com>
2021-12-24 15:35:12 +0300

    @/components/CustomTr: create

---
+++ src/components/CustomTr.vue
@@ -0,0 +1,54 @@
+
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+const props = {
+  head: {
+    type: Boolean,
+    default: false,
+  },
+  filter: {
+    type: Boolean,
+    default: false,
+  },
+};
+
+export default defineComponent({
+  render({ $slots, $attrs }: any) {
+    console.log(1);
+    const cells = $slots.default()
+      // .filter(({ type }: {type: string}): boolean => type === 'td')
+      // .map((td: any) => console.log(4, td));
+
+    // const cells = ($slots.default())
+    //   .filter(child => child.tag === 'td')
+    //   .map(td => {
+    //     if (props.head && td.data) {
+    //       td.data.staticClass = 'u-table_td_head';
+    //     }
+
+    //     return td;
+    //   })
+    // ;
+
+    console.log($slots.default());
+
+
+    return (
+      <tr
+        {...{
+          ...$attrs,
+          class: [
+            'table_tr',
+            {'table_tr_head': props.head},
+            {'table_tr_filter': props.filter},
+          ],
+        }}
+      >
+        {$slots.default()}
+      </tr>
+    )
+  }
+})
+</script>
+
