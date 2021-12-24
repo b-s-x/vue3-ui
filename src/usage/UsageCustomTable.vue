d9d9b1e4d3ab1f416b0682758bb056e0f92cc05c
b-s-x <b.s.x.mars@gmail.com>
2021-12-24 13:10:16 +0300

    @/usage/UsageCustomTable: add usage custom table

---
+++ src/usage/UsageCustomTable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CustomTable from '../components/CustomTable.vue';
+</script>
+
+<template>
+  <CustomTable
+    key="1"
+    level="7"
+    required
+  >
+    <tr>
+      11111
+    </tr>
+    <div>
+      22222
+    </div>
+    <tr>
+      3333
+    </tr>
+  </CustomTable>
+</template>
+
+<style>
+</style>
